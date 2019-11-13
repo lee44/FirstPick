911b50ad12c208072852ebd2b7247ce4b5dbb0bd
Lee44 <jlee7772@yahoo.com>
2019-11-13 14:13:01 -0800

    First Pick

---
+++ FirstPick/app/src/main/java/com/apps/jlee/firstpick/MainActivity.java
@@ -0,0 +1,15 @@
+package com.apps.jlee.firstpick;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(new MultitouchView(this));
+    }
+}
